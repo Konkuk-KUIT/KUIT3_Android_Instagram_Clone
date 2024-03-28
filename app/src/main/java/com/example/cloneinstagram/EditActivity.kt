32c406ee51edbc409017077438cfd4aa65f63fb1
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-03-28 11:37:46 +0900

    feat: 2주차 미션 완료

--- app/src/main/java/com/example/cloneinstagram/EditActivity.kt
+++ app/src/main/java/com/example/cloneinstagram/EditActivity.kt
@@ -5,16 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cloneinstagram.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityEditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_edit)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvEditName.text = intent.getStringExtra("nameValue")
+        binding.tvEditPostContent.text = intent.getStringExtra("contentValue")
+
+//        취소버튼 누르면 홈으로 돌아가기
+        binding.tvEditCancelBtn.setOnClickListener {
+            finish()
+        }
     }
 }
