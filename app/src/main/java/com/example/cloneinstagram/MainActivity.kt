6c71038dbf9c7c56ea7b760a2a43f85f07bd4eb6
hwidung <118262343+hwidung@users.noreply.github.com>
2024-03-28 23:01:01 +0900

    2주차 안드로이드 2조 조휘원

--- app/src/main/java/com/example/cloneinstagram/MainActivity.kt
+++ app/src/main/java/com/example/cloneinstagram/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cloneinstagram.R
+import com.example.cloneinstagram.SearchFragment
 import com.example.cloneinstagram.databinding.ActivityMainBinding
 
 
@@ -39,7 +40,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.reelsFragment -> {
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, ReelsFragment())
+                        .replace(R.id.main_frm, HomeFragment())
                         .commitAllowingStateLoss()
                     return@setOnItemSelectedListener true
                 }
