5a67f2a4d95f9d6e4fdc96959b1f610016233cd9
hwidung <hwhwhw74@naver.com>
2024-04-09 03:55:00 +0900

    4주차 미션 진행중

---
+++ app/src/main/java/com/example/cloneinstagram/TabFollowAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.cloneinstagram
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class followerAdapter:RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PostAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
