5cad8640703e46d8f24079b76a32ff73bad0f7d2
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-04-13 22:35:13 +0900

    Kuit3 android

--- app/src/main/java/com/example/cloneinstagram/ProfileFragment.kt
+++ app/src/main/java/com/example/cloneinstagram/ProfileFragment.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import com.example.cloneinstagram.R
 import com.example.cloneinstagram.databinding.FragmentProfileBinding
-import com.google.android.material.tabs.TabLayoutMediator
 
 class ProfileFragment : Fragment() {
     lateinit var binding: FragmentProfileBinding
-    private val tabList= arrayListOf(R.drawable.ic_grid,R.drawable.ic_tag)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,38 +16,6 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
-
-        val imageView: ImageView = binding.ivProfileImg
-        imageView.setImageResource(R.drawable.img_sample)
-
-        val profileData=arguments?.getSerializable("nameKey") as? PostData
-        binding.tvProfileId.text=profileData?.postId
-        binding.ivProfileImg.setImageResource(profileData!!.profileImgId)
-
-        binding.vpProfile.adapter=ProfileTabVPAdapter(this)
-        TabLayoutMediator(binding.tlGridTagTab,binding.vpProfile){ tab,position->
-            tab.setIcon(tabList[position])
-        }.attach()
-
-        binding.tvFollower.setOnClickListener {
-            val fragment=FollowListFragment()
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.main_frm,fragment)
-                .commit()
-            parentFragmentManager.executePendingTransactions()
-            fragment.binding.tlFwList.getTabAt(0)?.select()
-
-        }
-
-        binding.tvFollowing.setOnClickListener {
-            val fragment=FollowListFragment()
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.main_frm,fragment)
-                .commit()
-            parentFragmentManager.executePendingTransactions()
-            fragment.binding.tlFwList.getTabAt(1)?.select()
-        }
-
         return binding.root
     }
 }
